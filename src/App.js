940be78bbbdbeb22f9736764fc733283e203ceb3
joshi <joseph.ilagan.99@gmail.com>
2023-12-15 10:16:36 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 
 function App() {
@@ -11,6 +10,28 @@ function App() {
             <p>ABC Company</p>
             <p>Massachusetts</p>
             <p>Php150,000 - Php200,000 per month</p>
+            <ul className='list'>
+              <li> Flexible working hours</li>
+              <li> Work from home</li>
+              <li> High pay, high growth opportunity</li>
+            </ul>
+          </div>
+          <div className='leftCard1'>
+            <h1>Full Stack Developer</h1>
+            <p>Techno Company</p>
+            <p>Portugal</p>
+            <p>Php100,000 - Php120,000 per month</p>
+            <ul className='list'>
+              <li> Flexible working hours</li>
+              <li> Work from home</li>
+              <li> High pay, high growth opportunity</li>
+            </ul>
+          </div>
+          <div className='leftCard1'>
+            <h1>UI/UX Developer</h1>
+            <p>Hi-tech Company</p>
+            <p>Massachusetts</p>
+            <p>Php60,000 - Php70,000 per month</p>
             <ul className='list'>
               <li> Flexible working hours</li>
               <li> Work from home</li>
