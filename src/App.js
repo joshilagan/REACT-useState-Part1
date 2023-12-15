b751d1977490b9a896314d9816e1cfa419c2b90d
joshi <joseph.ilagan.99@gmail.com>
2023-12-15 09:53:14 +0800

    .

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
-      
+        <section className='leftsyd'>
+          <div>
+            <h1>Sr. React Front-end Developer</h1>
+            <p>ABC Company</p>
+            <p>Massachusetts</p>
+            <p>Php150,000-Php200,000 per month</p>
+            <ul className='list'>
+              <li> Flexible working hours</li>
+              <li> Work from home</li>
+              <li> High pay, high growth opportunity</li>
+            </ul>
+          </div>
+        </section>
+        <section className='rightsyd'>
+
+        </section>
     </div>
   );
 }
